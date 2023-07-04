54cfcc1bf52013054132607f6d3c01d493710cdc
jcw1031 <jcw001031@gmail.com>
2023-07-04 19:32:53 +0900

    JDBC H2 Connection

---
+++ jdbc/src/main/java/com/woopaca/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package com.woopaca.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "SA";
+    public static final String PASSWORD = "";
+}

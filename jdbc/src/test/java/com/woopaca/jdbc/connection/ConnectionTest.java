df687e98adcfb094e4f8ac340b059e289a1da259
jcw1031 <jcw001031@gmail.com>
2023-07-06 22:10:17 +0900

    Connection Pool 사용

--- jdbc/src/test/java/com/woopaca/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/com/woopaca/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.woopaca.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -27,8 +28,22 @@ public class ConnectionTest {
     @Test
     void dataSourceDriverManager() throws SQLException {
         // DriverManagerDataSource - 항상 새로운 커넥션 획득
-        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // Connection Pooling
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("Test Pool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1_000);
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
